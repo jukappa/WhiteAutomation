af66aec96684c97e49fcc30d59a2d9a1b290689c
Julian Lobert <c-julian.lobert@charter.com>
2017-10-30 15:55:41 -0600

    removed unneccesary code in pretest

--- src/main/java/PreTest.java
+++ src/main/java/PreTest.java
@@ -9,47 +9,46 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class PreTest {
     static ExtentTest test;
+
     public static WebDriver getBrowserDrivers(String browserNames) {
-        String[] browser = {browserNames};
-        for (String browserCycle : browser) {
-            String name = null;
-            String path = null;
-            WebDriver driver = null;
-            switch (browserCycle.toUpperCase()) {
-                case "CHROME":
-                    name = "webdriver.chrome.driver";
-                    //Gets value in environment variables(uses directory path to specific driver)
-                    path = System.getenv("CHROME_DRIVER");
-                    System.setProperty(name, path);
-                    return new ChromeDriver();
-                case "FIREFOX":
-                case "FIRE FOX":
-                    name = "webdriver.gecko.driver";
-                    path = System.getenv("FIREFOX_DRIVER");
-                    System.setProperty(name, path);
-                    return new FirefoxDriver();
-                case "IE":
-                case "INTERNET EXPLORER":
-                    name = "webdriver.ie.driver";
-                    path = System.getenv("IE_DRIVER");
-                    DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
-                    caps.setCapability("ignoreZoomSetting", true);
-                    System.setProperty(name, path);
-                    return new InternetExplorerDriver(caps);
 
-                case "EDGE":
-                    name = "webdriver.edge.driver";
-                    path = System.getenv("EDGE_DRIVER");
-                    System.setProperty(name, path);
-                    return new EdgeDriver();
+        String name = null;
+        String path = null;
+        WebDriver driver = null;
+        switch (browserNames.toUpperCase()) {
+            case "CHROME":
+                name = "webdriver.chrome.driver";
+                //Gets value in environment variables(uses directory path to specific driver)
+                path = System.getenv("CHROME_DRIVER");
+                System.setProperty(name, path);
+                return new ChromeDriver();
+            case "FIREFOX":
+            case "FIRE FOX":
+                name = "webdriver.gecko.driver";
+                path = System.getenv("FIREFOX_DRIVER");
+                System.setProperty(name, path);
+                return new FirefoxDriver();
+            case "IE":
+            case "INTERNET EXPLORER":
+                name = "webdriver.ie.driver";
+                path = System.getenv("IE_DRIVER");
+                DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
+                caps.setCapability("ignoreZoomSetting", true);
+                System.setProperty(name, path);
+                return new InternetExplorerDriver(caps);
 
-                default:
-                    System.out.println("Ensure entered browser is one of the following 'Chrome','Firefox','edge','IE'");
+            case "EDGE":
+                name = "webdriver.edge.driver";
+                path = System.getenv("EDGE_DRIVER");
+                System.setProperty(name, path);
+                return new EdgeDriver();
 
-            }
-            test.log(Status.PASS, browserCycle +" driver set");
+            default:
+                System.out.println("Ensure entered browser is one of the following 'Chrome','Firefox','edge','IE'");
+
         }
+        test.log(Status.PASS, browserNames + " driver set");
+
         return null;
     }
-
 }
